7738ac0314f7f3eab2e04d7ef7567359a25d1baf
ElodieAllegro <124352648+ElodieAllegro@users.noreply.github.com>
2025-04-07 09:28:57 +0200

    erreur counter

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -74,12 +74,9 @@ export default class {
     this.store = store
     this.counterShow = undefined
     this.counterEdit = undefined
-     $('#arrow-icon1').on('click', (e) => this.handleShowTickets(e, bills, 1))
-     $('#arrow-icon2').on('click', (e) => this.handleShowTickets(e, bills, 2))
-     $('#arrow-icon3').on('click', (e) => this.handleShowTickets(e, bills, 3))
-    // $('#arrow-icon1').click((e) => this.handleShowTickets(e, bills, 1))
-    // $('#arrow-icon2').click((e) => this.handleShowTickets(e, bills, 2))
-    // $('#arrow-icon3').click((e) => this.handleShowTickets(e, bills, 3))
+    $('#arrow-icon1').on('click', (e) => this.handleShowTickets(e, bills, 1))
+    $('#arrow-icon2').on('click', (e) => this.handleShowTickets(e, bills, 2))
+    $('#arrow-icon3').on('click', (e) => this.handleShowTickets(e, bills, 3))
     new Logout({ localStorage, onNavigate })
   }
 
@@ -87,16 +84,12 @@ export default class {
     const billUrl = $('#icon-eye-d').attr("data-bill-url")
     const imgWidth = Math.floor($('#modaleFileAdmin1').width() * 0.8)
     $('#modaleFileAdmin1').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} alt="Bill"/></div>`)
-  
     if (typeof $('#modaleFileAdmin1').modal === 'function') $('#modaleFileAdmin1').modal('show')
   }
 
   handleEditTicket(e, bill, bills) {
-    // if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.counterEdit === undefined || this.id !== bill.id) this.counterEdit = 0
-
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    // if (this.counter % 2 === 0) {
     if (this.counterEdit % 2 === 0) {
       bills.forEach(b => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
@@ -104,7 +97,6 @@ export default class {
       $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
-      // this.counter ++
       this.counterEdit ++
     } else {
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
@@ -113,12 +105,8 @@ export default class {
         <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
       `)
       $('.vertical-navbar').css({ height: '120vh' })
-      // this.counter ++
       this.counterEdit ++
     }
-    // $('#icon-eye-d').click(this.handleClickIconEye)
-    // $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
-    // $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
     $('#icon-eye-d').on('click', this.handleClickIconEye)
     $('#btn-accept-bill').on('click', (e) => this.handleAcceptSubmit(e, bill))
     $('#btn-refuse-bill').on('click', (e) => this.handleRefuseSubmit(e, bill))
@@ -145,26 +133,24 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    // if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.counterShow === undefined || this.index !== index) this.counterShow = 0
     if (this.index === undefined || this.index !== index) this.index = index
-    // if (this.counter % 2 === 0) {
     if (this.counterShow % 2 === 0) {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
       $(`#status-bills-container${this.index}`)
         .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
+      this.counterShow ++
     } else {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
       $(`#status-bills-container${this.index}`)
         .html("")
-      // this.counter ++
       this.counterShow ++
     }
+
     bills.forEach(bill => {    
-        $(`#open-bill${bill.id}`).on('click', (e) => {
-          this.handleEditTicket(e, bill, bills)
-        })
+      $(`#open-bill${bill.id}`).on('click', (e) => {
+        this.handleEditTicket(e, bill, bills)
+      })
     })
 
     return bills
